dcc06c71ceea232374972cea374d85adbe01812d
mailech <mm@ar.co.il>
2026-08-24 11:37:04 +0300

    Add find_student function

--- manager.py
+++ manager.py
@@ -1,14 +1,25 @@
 import csv
 import os 
-def load_students(filename):
-    if not os.path.exists (filename):
-        return []
-    with open(filename, "r" ,encoding="utf-8",newline="") as f:
-        
-        a=csv.DictReader(f)
-    return list(a)
 
-def add_student(students, name, grade, class_name):
-    with open(students,"a",encoding="utf-8",newline="") as f:
-        c=csv.writer(f)
+def load_students(filename):
+    if not os.path.exists(filename):
+        return []
+    with open(filename, "r", encoding="utf-8", newline="") as f:
+        a = csv.DictReader(f)
+        return list(a)
+
+def add_student(filename, name, grade, class_name):
+    with open(filename, "a", encoding="utf-8", newline="") as f:
+        c = csv.writer(f)
         c.writerow([name, grade, class_name])
+
+def find_student(students, name):
+    for name1 in students:
+        if name1.get("name") == name:
+            return name1
+    else:
+        return None  # ה-else הזה חייב להיות מיושר עם ה-for
+
+n = load_students("students.csv")
+a = find_student(n, "Noa")
+print(a)
